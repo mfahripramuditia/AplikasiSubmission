bf77a1b6b07551c1110156d9bda790ed104bb76d
mfahripramuditia <mfahripramuditia@gmail.com>
2022-02-10 08:55:45 +0700

    update

--- app/src/main/java/com/tutorial/deliserdangapps/DetailActivity.java
+++ app/src/main/java/com/tutorial/deliserdangapps/DetailActivity.java
@@ -27,7 +27,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowCustomEnabled(true);
